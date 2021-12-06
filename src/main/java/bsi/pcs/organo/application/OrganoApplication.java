f22199211155502750f0fdfc14c57f6a925fb291
Rafael Lima <rafalimajo@gmail.com>
2021-12-06 19:37:12 -0300

    Add CORS configuration.

--- src/main/java/bsi/pcs/organo/application/OrganoApplication.java
+++ src/main/java/bsi/pcs/organo/application/OrganoApplication.java
@@ -3,15 +3,29 @@ package bsi.pcs.organo.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories("bsi.pcs.organo.repository")
 @EntityScan("bsi.pcs.organo.entity")
 @ComponentScan(basePackages = "bsi.pcs.organo")
 public class OrganoApplication {
+	
     public static void main(String[] args) {
         SpringApplication.run(OrganoApplication.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
